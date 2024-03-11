64e03f3ae69596c670c53e582b9ca13b0c224bd8
sc-johanbecue <johan.becue@sitecore.com>
2024-03-11 16:37:42 +0100

    modified:   src/sxastarter/src/LayoutDPWorld.tsx

--- src/sxastarter/src/LayoutDPWorld.tsx
+++ src/sxastarter/src/LayoutDPWorld.tsx
@@ -98,7 +98,9 @@ const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
           <div id="header">{route && <Placeholder name="headless-header" rendering={route} />}</div>
         </header>
         <main>
-          <div id="content">{route && <Placeholder name="headless-main" rendering={route} />}</div>
+          <div id="content" className="main-container">
+            {route && <Placeholder name="headless-main" rendering={route} />}
+          </div>
         </main>
         <footer>
           <div id="footer">{route && <Placeholder name="headless-footer" rendering={route} />}</div>
